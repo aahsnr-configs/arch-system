620d85e47a22b92a628b0e32b24797c3c281417d
aahsnr-configs <ahsanur041@proton.me>
2025-11-12 04:45:27 -0500

    removed dms

--- dotfiles/.config/nvim/lua/plugins/nix.lua
+++ dotfiles/.config/nvim/lua/plugins/nix.lua
@@ -9,17 +9,6 @@ return {
     end,
   },
   {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local builtins = require("null-ls").builtins
-      opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-        builtins.code_actions.statix,
-        builtins.diagnostics.deadnix,
-      })
-    end,
-  },
-  {
     "AstroNvim/astrolsp",
     optional = true,
     opts = function(_, opts) opts.servers = require("astrocore").list_insert_unique(opts.servers, { "nil_ls" }) end,
