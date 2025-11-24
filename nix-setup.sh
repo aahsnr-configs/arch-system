bf5ebd031cd68c64319a28bc337a319273a98bb4
aahsnr-configs <ahsanur041@proton.me>
2025-11-25 05:14:47 +0600

    updated

--- nix-setup.sh
+++ nix-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
-#sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+# curl -fsSL https://install.determinate.systems/nix | sh -s -- install --determinate
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
 #nix run home-manager/master -- init --switch
