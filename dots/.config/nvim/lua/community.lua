39d697b11c8468cbdb61555e4fe83ec7396c4935
aahsnr-configs <ahsanur041@proton.me>
2026-01-04 22:59:27 +0600

    updated

--- dots/.config/nvim/lua/community.lua
+++ dots/.config/nvim/lua/community.lua
@@ -10,7 +10,7 @@ return {
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
-  -- { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.prettier" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python" },
