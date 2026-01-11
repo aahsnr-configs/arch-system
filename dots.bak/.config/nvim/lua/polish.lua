35c9001dd9443961ea3ce8d7b3ea64849241b402
aahsnr-configs <ahsanur041@proton.me>
2026-01-12 03:41:24 +0600

    remove backup dots

--- dots.bak/.config/nvim/lua/polish.lua
+++
@@ -1,9 +0,0 @@
---if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  vim.api.nvim_set_keymap("x", "p", '"_dp', { noremap = true, silent = true }),
-}
-
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
