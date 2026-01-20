b498fb451b79cc68e20b662ba724d843d7c64ba1
aahsnr-configs <ahsanur041@proton.me>
2026-01-20 17:36:26 +0600

    updated

--- dots/.config/nvim/lua/plugins/markdown.lua
+++ dots/.config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,54 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    cmd = "RenderMarkdown",
+    ft = function()
+      local plugin = require("lazy.core.config").spec.plugins["render-markdown.nvim"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      return opts.file_types or { "markdown" }
+    end,
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+          if opts.ensure_installed ~= "all" then
+            opts.ensure_installed =
+              require("astrocore").list_insert_unique(opts.ensure_installed, { "html", "markdown", "markdown_inline" })
+          end
+        end,
+      },
+    },
+    opts = {},
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    ft = { "markdown", "markdown.mdx" },
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    init = function()
+      local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
+      vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
+    end,
+    dependencies = {
+      { "AstroNvim/astroui", opts = { icons = { Markdown = "" } } },
+      {
+        "AstroNvim/astrocore",
+        optional = true,
+        opts = function(_, opts)
+          local maps = opts.mappings
+          local prefix = "<Leader>M"
+
+          maps.n[prefix] = { desc = require("astroui").get_icon("Markdown", 1, true) .. "Markdown" }
+          maps.n[prefix .. "p"] = { "<cmd>MarkdownPreview<cr>", desc = "Preview" }
+          maps.n[prefix .. "s"] = { "<cmd>MarkdownPreviewStop<cr>", desc = "Stop preview" }
+          maps.n[prefix .. "t"] = { "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle preview" }
+        end,
+      },
+    },
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
@@ -22,14 +71,5 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "marksman" })
     end,
-  },
-
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    opts = {},
   },
 }
